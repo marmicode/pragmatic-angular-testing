3657e6cfcdd50486083be3a2be9833859194314b
Younes Jaaidi <younes@marmicode.io>
2025-12-11 20:28:19 +0100

    refactor: 🛠️ tidy up

--- tools/cook/infra.ts
+++ tools/cook/infra.ts
@@ -1,4 +1,4 @@
-import { execSync } from 'child_process';
+import { execSync } from 'node:child_process';
 import inquirer from 'enquirer';
 import { readdirSync, readFileSync, rmSync, writeFileSync } from 'fs';
 const { prompt } = inquirer;
