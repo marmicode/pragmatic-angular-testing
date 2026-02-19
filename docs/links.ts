c58c329a06345d06c262b6ef8bce95d921405234
Younes Jaaidi <younes@marmicode.io>
2026-02-19 14:37:08 +0100

    docs: 📝 remove expired gifts

--- docs/links.ts
+++ docs/links.ts
@@ -1,3 +1,2 @@
-const todayCoupon = 'NGPOLAND_PERIOGI';
-export const courseLink = `https://courses.marmicode.io/courses/pragmatic-angular-testing?utm_source=cookbook&utm_medium=sidenav&coupon=${todayCoupon}`;
-export const workshopLink = `https://luma.com/27f85isx?coupon=${todayCoupon}`;
+export const courseLink = `https://courses.marmicode.io/courses/pragmatic-angular-testing?utm_source=cookbook&utm_medium=sidenav`;
+export const workshopLink = `https://marmicode.io/workshops?utm_source=cookbook&utm_medium=sidenav`;
