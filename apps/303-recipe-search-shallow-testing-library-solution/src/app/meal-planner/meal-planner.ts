75e111ef2d4363b48c46b136d3e4164c97e1fa38
Younes Jaaidi <younes@marmicode.io>
2026-03-26 17:23:41 +0100

    fixup! feat: ✨ add 103-meal-planner-signal-based exercise

--- apps/303-recipe-search-shallow-testing-library-solution/src/app/meal-planner/meal-planner.ts
+++ apps/303-recipe-search-shallow-testing-library-solution/src/app/meal-planner/meal-planner.ts
@@ -1,71 +1,38 @@
-import { inject, Injectable } from '@angular/core';
-import {
-  BehaviorSubject,
-  merge,
-  mergeMap,
-  Observable,
-  Subject,
-  tap,
-} from 'rxjs';
-import { distinctUntilChanged, map } from 'rxjs/operators';
+import { inject, Injectable, signal, untracked } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { lastValueFrom } from 'rxjs';
 import { Recipe } from '../recipe/recipe';
 import { MealRepository } from './meal-repository';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MealPlanner {
-  recipes$: Observable<Recipe[]>;
+  private _recipes = signal<Recipe[]>([]);
+  recipes = this._recipes.asReadonly();
 
   private _mealRepository = inject(MealRepository);
-  private _recipes$ = new BehaviorSubject<Recipe[]>([]);
-  private _addRecipe$ = new Subject<Recipe>();
 
   constructor() {
-    this.recipes$ = this._recipes$.asObservable();
-
-    const effects = [
-      /* Fetch meals from repository. */
-      this._mealRepository
-        .getMeals()
-        .pipe(tap((recipes) => this._recipes$.next(recipes))),
-      /* Add meals to repository. */
-      this._addRecipe$.pipe(
-        mergeMap((recipe) =>
-          this._mealRepository.addMeal(recipe).pipe(map(() => recipe)),
-        ),
-        tap((recipe) => this._recipes$.next([...this._recipes$.value, recipe])),
-      ),
-    ];
-
-    merge(...effects)
+    this._mealRepository
+      .getMeals()
       .pipe(takeUntilDestroyed())
-      .subscribe();
+      .subscribe((recipes) => {
+        this._recipes.set(recipes);
+      });
   }
 
-  addRecipe(recipe: Recipe) {
-    if (!this._canAddRecipe({ recipe, recipes: this._recipes$.value })) {
+  canAddRecipe(recipe: Recipe): boolean {
+    return this.recipes().find((r) => recipe.id === r.id) == null;
+  }
+
+  async addRecipe(recipe: Recipe) {
+    if (!untracked(() => this.canAddRecipe(recipe))) {
       throw new Error(`Can't add recipe.`);
     }
 
-    this._addRecipe$.next(recipe);
-  }
+    await lastValueFrom(this._mealRepository.addMeal(recipe));
 
-  watchCanAddRecipe(recipe: Recipe): Observable<boolean> {
-    return this._recipes$.pipe(
-      map((recipes) => this._canAddRecipe({ recipe, recipes })),
-      distinctUntilChanged(),
-    );
-  }
-
-  private _canAddRecipe({
-    recipes,
-    recipe,
-  }: {
-    recipes: Recipe[];
-    recipe: Recipe;
-  }) {
-    return !recipes.find((_recipe) => _recipe.id === recipe.id);
+    this._recipes.update((recipes) => [...recipes, recipe]);
   }
 }
